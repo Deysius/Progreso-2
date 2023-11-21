5aaffa558a9eaf6681db63ff057ef1a50b6f8657
Josue Ayala <josue.ayala.flores@udla.edu.ec>
2023-11-21 15:04:13 -0500

    Utilizo la misma sintaxis del ejercicio hecho en clase, para obtener la matriz con ceros

--- Ejercicio Matrix.c
+++ Ejercicio Matrix.c
@@ -1,4 +1,17 @@
 #include <stdio.h>
 int main(){
-return 0;
+    int filas, columnas;
+    printf("Ingrese el número de filas de la matriz: ");
+    scanf("%d", &filas);
+    printf("Ingrese el número de columnas de la matriz: ");
+    scanf("%d", &columnas);
+    printf("Matriz con ceros:\n");
+    int matriz[filas][columnas];
+    for (int i = 0; i < filas; i++) {
+        for (int j = 0; j < columnas; j++) {
+            matriz[i][j] = 0;
+            printf("%d ", matriz[i][j]);
+        }
+          printf("\n");
+    }
 }
